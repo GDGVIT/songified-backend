43e0ab613bf79064b8a867d9098d625623853922
Rehaan Mazid <rehaanmazid@gmail.com>
2021-03-02 12:54:57 +0530

    fix: fix lint standard

--- api/routes/search.js
+++ api/routes/search.js
@@ -1,132 +1,114 @@
-const express = require('express');
-const https = require("https");
-const router = express.Router();
+const express = require('express')
+const https = require('https')
+const router = express.Router()
 
-router.post('/song_and_artist',(req,res)=>{
-    
-        if(!req.body.song_name)
-        {
-            return res.status(400).json({
-                error:"missing required parameters. refer documentation"
-            });
-        }
+router.post('/song_and_artist', (req, res) => {
+  if (!req.body.song_name) {
+    return res.status(400).json({
+      error: 'missing required parameters. refer documentation'
+    })
+  }
 
-        if(!req.body.artist)
-        {
-            return res.status(400).json({
-                error:"missing required parameters. refer documentation"
-            });
-        }
+  if (!req.body.artist) {
+    return res.status(400).json({
+      error: 'missing required parameters. refer documentation'
+    })
+  }
 
-        const song_name = req.body.song_name.split(' ').join('+');
-        const artist = req.body.artist.split(' ').join('+');
-        const url = "https://api.getsongbpm.com/search/?api_key="+process.env.API_KEY+"&type=both&lookup=song:"+song_name+" artist:"+artist;
+  const songName = req.body.song_name.split(' ').join('+')
+  const artist = req.body.artist.split(' ').join('+')
+  const url = 'https://api.getsongbpm.com/search/?api_key=' + process.env.API_KEY + '&type=both&lookup=song:' + songName + ' artist:' + artist
 
-        https.get(url,(response)=>{
-            console.log(response.statusCode);
-            
-            var body = '';
+  https.get(url, (response) => {
+    console.log(response.statusCode)
 
-            response.on('data', function(data){
-                body += data;
-            });
+    let body = ''
 
-            response.on("end",()=>
-            {
-                const song_data = JSON.parse(body);
-                if(song_data.search.error)
-                {
-                    return res.status(200).json({
-                        error: song_data.search.error
-                    });
-                }
-                const song = song_data.search[0];
-                const song_tempo = song.tempo;
-                const song_key = song.key_of;
-                const song_time_sig = song.time_sig;
-                res.status(200).json({
-                    details : 
+    response.on('data', function (data) {
+      body += data
+    })
+
+    response.on('end', () => {
+      const songData = JSON.parse(body)
+      if (songData.search.error) {
+        return res.status(200).json({
+          error: songData.search.error
+        })
+      }
+      const song = songData.search[0]
+      const songTempo = song.tempo
+      const songKey = song.key_of
+      const songTimeSig = song.time_sig
+      res.status(200).json({
+        details:
                     {
-                        song_tempo : song_tempo,
-                        song_key : song_key,
-                        song_time_sig : song_time_sig
+                      song_tempo: songTempo,
+                      song_key: songKey,
+                      song_time_sig: songTimeSig
                     }
-                });
-            });
-            
-        });
-        
+      })
+    })
+  })
+})
 
+router.post('/song', (req, res) => {
+  if (!req.body.song_name) {
+    res.status(400).json({
+      error: 'missing required parameter. refer documentation'
+    })
+  }
 
+  const songName = req.body.song_name.split(' ').join('+')
+  const url = 'https://api.getsongbpm.com/search/?api_key=' + process.env.API_KEY + '&type=song&lookup=' + songName
 
-});
+  https.get(url, (response) => {
+    console.log(response.statusCode)
 
-router.post("/song",(req,res)=>{
-    if(!req.body.song_name)
-    {
-        res.status(400).json({
-            error: "missing required parameter. refer documentation"
-        });
-    }
+    let body = ''
 
-    const song_name = req.body.song_name.split(' ').join('+');
-    const url = "https://api.getsongbpm.com/search/?api_key="+process.env.API_KEY+"&type=song&lookup="+song_name;
+    response.on('data', (data) => {
+      body += data
+    })
 
-    https.get(url,(response)=>{
-        console.log(response.statusCode);
+    response.on('end', () => {
+      const songData = JSON.parse(body)
+      const song = songData.search[0]
 
-        var body = '';
-        
-        response.on("data",(data)=>{
-            body +=data;
-        });
+      const songId = song.id
 
-        response.on("end",()=>{
-            
-            const song_data = JSON.parse(body);
-            const song = song_data.search[0];
-            
-            const song_id = song.id;
-        
-            const url2 = "https://api.getsongbpm.com/song/?api_key="+process.env.API_KEY+"&id="+song_id;
+      const url2 = 'https://api.getsongbpm.com/song/?api_key=' + process.env.API_KEY + '&id=' + songId
 
-            https.get(url2,(response)=>{
-                console.log(response.statusCode);
+      https.get(url2, (response) => {
+        console.log(response.statusCode)
 
-                var body2='';
+        let body2 = ''
 
-                response.on("data",(data)=>{
-                    body2 += data;
-                });
+        response.on('data', (data) => {
+          body2 += data
+        })
 
-                response.on("end",()=>{
+        response.on('end', () => {
+          const songDetails = JSON.parse(body2)
+          const allDetails = songDetails.song
 
-                    const song_details = JSON.parse(body2);
-                    const all_details = song_details.song;
-                    
-                    const song_artist = all_details.artist.name;
-                    const song_tempo = all_details.tempo;
-                    const song_time_sig = all_details.time_sig;
-                    const song_key = all_details.key_of;
+          const songArtist = allDetails.artist.name
+          const songTempo = allDetails.tempo
+          const songTimeSig = allDetails.time_sig
+          const songKey = allDetails.key_of
 
-                    res.status(200).json({
-                        details : 
+          res.status(200).json({
+            details:
                         {
-                            artist : song_artist,
-                            song_tempo : song_tempo,
-                            song_key : song_key,
-                            song_time_sig : song_time_sig
+                          artist: songArtist,
+                          song_tempo: songTempo,
+                          song_key: songKey,
+                          song_time_sig: songTimeSig
                         }
-                    });
+          })
+        })
+      })
+    })
+  })
+})
 
-
-                });
-            });
-
-        });
-
-    });
-});
-
-
-module.exports = router;
+module.exports = router
--- app.js
+++ app.js
@@ -1,41 +1,37 @@
-require('dotenv').config();
-const express = require("express");
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
-const searchRoute = require("./api/routes/search");
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const searchRoute = require('./api/routes/search')
 
-const app = express();
+const app = express()
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
-app.use("/search",searchRoute);
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use('/search', searchRoute)
 
-//adding required headers to prevent CORS(Cross Origin Resourse Sharin) Error
-app.use((req,res,next)=>
-{
-    res.header("Access-Control-Allow-Origin","*");
-    res.header("Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept,Authorization");
-    if(req.method === "OPTIONS")
-    {
-        res.header("Access-Control-Allow-Methods","PUT,POST,PATCH,DELETE,GET");
-        return res.status(200).json({});
-    }
-    next();
-});
+// adding required headers to prevent CORS(Cross Origin Resourse Sharin) Error
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,Authorization')
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET')
+    return res.status(200).json({})
+  }
+  next()
+})
 
-app.get('/',(req, res) => {
-  
-    res.status(200).json({
-      status: 200,
-      message: "Songify API Up and Running, Refer Documentation for Use"
-    });
-  
-  });
-  
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status: 200,
+    message: 'Songify API Up and Running, Refer Documentation for Use'
+  })
+})
+
 app.listen(port, () => {
-    console.log("Server Up and Running at Port");
-  });
+  console.log('Server Up and Running at Port')
+})
