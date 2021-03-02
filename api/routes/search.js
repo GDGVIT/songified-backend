964db5e2f841234f9f2872c005cf3d9a957a79df
Rehaan Mazid <rehaanmazid@gmail.com>
2021-03-02 10:27:39 +0530

    feat(search): add search by song name

--- api/routes/search.js
+++ api/routes/search.js
@@ -23,7 +23,6 @@ router.post('/song_and_artist',(req,res)=>{
         const url = "https://api.getsongbpm.com/search/?api_key="+process.env.API_KEY+"&type=both&lookup=song:"+song_name+" artist:"+artist;
 
         https.get(url,(response)=>{
-            console.log(url);
             console.log(response.statusCode);
             
             var body = '';
@@ -60,6 +59,73 @@ router.post('/song_and_artist',(req,res)=>{
 
 
 
+});
+
+router.post("/song",(req,res)=>{
+    if(!req.body.song_name)
+    {
+        res.status(400).json({
+            error: "missing required parameter. refer documentation"
+        });
+    }
+
+    const song_name = req.body.song_name.split(' ').join('+');
+    const url = "https://api.getsongbpm.com/search/?api_key="+process.env.API_KEY+"&type=song&lookup="+song_name;
+
+    https.get(url,(response)=>{
+        console.log(response.statusCode);
+
+        var body = '';
+        
+        response.on("data",(data)=>{
+            body +=data;
+        });
+
+        response.on("end",()=>{
+            
+            const song_data = JSON.parse(body);
+            const song = song_data.search[0];
+            
+            const song_id = song.id;
+        
+            const url2 = "https://api.getsongbpm.com/song/?api_key="+process.env.API_KEY+"&id="+song_id;
+
+            https.get(url2,(response)=>{
+                console.log(response.statusCode);
+
+                var body2='';
+
+                response.on("data",(data)=>{
+                    body2 += data;
+                });
+
+                response.on("end",()=>{
+
+                    const song_details = JSON.parse(body2);
+                    const all_details = song_details.song;
+                    
+                    const song_artist = all_details.artist.name;
+                    const song_tempo = all_details.tempo;
+                    const song_time_sig = all_details.time_sig;
+                    const song_key = all_details.key_of;
+
+                    res.status(200).json({
+                        details : 
+                        {
+                            artist : song_artist,
+                            song_tempo : song_tempo,
+                            song_key : song_key,
+                            song_time_sig : song_time_sig
+                        }
+                    });
+
+
+                });
+            });
+
+        });
+
+    });
 });
 
 
