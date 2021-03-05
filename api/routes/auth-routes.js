b7c1a019329d59f06d976fcf3d30ce6562f9bce7
Rehaan Mazid <rehaanmazid@gmail.com>
2021-03-05 21:42:24 +0530

    feat: auth route added

---
+++ api/routes/auth-routes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router()
+const passport = require('passport')
+
+router.get('/logout', (req, res) => {
+  res.logout()
+  res.status(200).json({
+    message: 'logout successful'
+  })
+})
+
+router.get('/google', passport.authenticate('google', {
+  scope: ['profile']
+}))
+
+router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
+  res.status(200).json({
+    message: 'login succesful, cookie created',
+    data: {
+      name: req.user.username,
+      points: req.user.points
+    }
+  })
+})
+
+module.exports = router
