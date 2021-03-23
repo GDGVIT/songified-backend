b021dcfa04a1f663d4c0399716c789977dd899dc
Rehaan Mazid <rehaanmazid@gmail.com>
2021-03-23 12:50:27 +0530

    fix: lint

--- api/routes/upload.js
+++ api/routes/upload.js
@@ -1,12 +1,12 @@
 const router = require('express').Router()
 const authCheck = require('./authCheck')
 
-router.get('/',(req,res)=>{
-    res.json("GET ACCESSED OF UPLOAD")
+router.get('/', authCheck, (req, res) => {
+  res.json('GET ACCESSED OF UPLOAD')
 })
 
-router.post('/cyaniteWebHook',(req,res)=>{
-    console.log(req);
-});
+router.post('/cyaniteWebHook', (req, res) => {
+  console.log(req)
+})
 
 module.exports = router
