04da0c52f4a1f8eda3ac5403b8cb91ea0fdf996c
Rehaan Mazid <rehaanmazid@gmail.com>
2021-04-05 22:47:36 +0530

    feat: update get request

--- api/routes/upload.js
+++ api/routes/upload.js
@@ -98,60 +98,9 @@ const updateAnalysisData = async function updateAnalysisData (songId) {
 
 // @TODO Remove this
 router.get('/', (req, res) => {
-  const data = JSON.stringify({
-    query: `query analysis($id: ID!) {
-      inDepthAnalysis(recordId: $id) {
-        __typename
-        ... on Error {
-          message
-        }
-        ... on InDepthAnalysis {
-          id
-          title
-          fullScaleMusicalAnalysis {
-            __typename
-            ... on FullScaleMusicalAnalysisFailed {
-              error {
-                __typename
-                ... on Error {
-                  message
-                }
-              }
-            }
-            ... on FullScaleMusicalAnalysisFinished {
-              result {
-                bpm
-                key {
-                  values
-                  confidences
-                }
-              }
-            }
-          }
-        }
-      }
-    }`,
-    variables: { id: 1294396 }
+  res.status(200).json({
+    message: 'refer documentation'
   })
-
-  const config = {
-    method: 'post',
-    url: 'https://app.cyanite.ai/graphql',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: 'Bearer ' + process.env.CYANITE_ACCESS_TOKEN
-    },
-    data: data
-  }
-
-  axios(config)
-    .then(function (response) {
-      console.log(JSON.stringify(response.data))
-      res.status(200).json(response.headers)
-    })
-    .catch(function (error) {
-      console.log(error)
-    })
 })
 
 router.post('/cyaniteWebHook', (req, res) => {
