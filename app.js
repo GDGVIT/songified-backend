df57c007b13caa75665d5392115520112864049e
Rehaan Mazid <rehaanmazid@gmail.com>
2021-05-08 13:45:27 +0530

    fix: fixing cors issue

--- app.js
+++ app.js
@@ -34,17 +34,6 @@ app.use('/userInfo', userInfoRoute)
 app.use('/upload', uploadRoute)
 
 // adding required headers to prevent CORS(Cross Origin Resourse Sharin) Error
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers',
-    'Origin,X-Requested-With,Content-Type,Accept,Authorization')
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET')
-    return res.status(200).json({})
-  }
-  next()
-})
-
 app.use(cors())
 
 app.get('/', (req, res) => {
